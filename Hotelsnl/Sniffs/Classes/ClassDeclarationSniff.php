1ac6872fb3771506def275b5a9f4e80574484a2e
Martijn Swaagman <martijn@hotels.nl>
2012-04-25 09:57:19 +0200

    better class dec

--- Hotelsnl/Sniffs/Classes/ClassDeclarationSniff.php
+++ Hotelsnl/Sniffs/Classes/ClassDeclarationSniff.php
@@ -32,9 +32,15 @@ if (class_exists('PEAR_Sniffs_Classes_ClassDeclarationSniff', true) === false) {
  * @version   Release: 1.3.3
  * @link      http://pear.php.net/package/PHP_CodeSniffer
  */
-class Hotelsnl_Sniffs_Classes_ClassDeclarationSniff extends PEAR_Sniffs_Classes_ClassDeclarationSniff
+class Hotelsnl_Sniffs_Classes_ClassDeclarationSniff implements PHP_CodeSniffer_Sniff
 {
 
+    /**
+     * The number of spaces code should be indented.
+     *
+     * @var int
+     */
+    public $indent = 2;
 
     /**
      * Returns an array of tokens this test wants to listen for.
@@ -50,6 +56,66 @@ class Hotelsnl_Sniffs_Classes_ClassDeclarationSniff extends PEAR_Sniffs_Classes_
 
     }//end register()
 
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param integer              $stackPtr  The position of the current token in the
+     *                                        stack passed in $tokens.
+     *
+     * @return void
+     */
+    private function braces(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens    = $phpcsFile->getTokens();
+        $errorData = array($tokens[$stackPtr]['content']);
+
+        if (isset($tokens[$stackPtr]['scope_opener']) === false) {
+            $error = 'Possible parse error: %s missing opening or closing brace';
+            $phpcsFile->addWarning($error, $stackPtr, 'MissingBrace', $errorData);
+            return;
+        }
+
+        $curlyBrace  = $tokens[$stackPtr]['scope_opener'];
+        $lastContent = $phpcsFile->findPrevious(T_WHITESPACE, ($curlyBrace - 1), $stackPtr, true);
+        $classLine   = $tokens[$lastContent]['line'];
+        $braceLine   = $tokens[$curlyBrace]['line'];
+        if ($braceLine !== $classLine) {
+            $error = 'Opening brace of a %s must be on the SAME line of the definition';
+            $phpcsFile->addError($error, $curlyBrace, 'OpenBraceNewLine', $errorData);
+            return;
+        } else if ($braceLine > ($classLine + 1)) {
+            $error = 'Opening brace of a %s must be on the SAME line as the %s declaration; found %s line(s)';
+            $data  = array(
+                      $tokens[$stackPtr]['content'],
+                      $tokens[$stackPtr]['content'],
+                      ($braceLine - $classLine - 1),
+                     );
+            $phpcsFile->addError($error, $curlyBrace, 'OpenBraceWrongLine', $data);
+            return;
+        }
+
+        if ($tokens[($curlyBrace - 1)]['code'] === T_WHITESPACE) {
+            $prevContent = $tokens[($curlyBrace - 1)]['content'];
+            if ($prevContent === $phpcsFile->eolChar) {
+                $spaces = 0;
+            } else {
+                $blankSpace = substr($prevContent, strpos($prevContent, $phpcsFile->eolChar));
+                $spaces     = strlen($blankSpace);
+            }
+
+            $expected = ($tokens[$stackPtr]['level'] * $this->indent) + 1;
+            if ($spaces !== $expected) {
+              $error = 'Expected %s spaces before opening brace; %s found';
+                $data  = array(
+                          $expected,
+                          $spaces,
+                         );
+                $phpcsFile->addError($error, $curlyBrace, 'SpaceBeforeBrace', $data);
+            }
+        }
+
+    }//end process()
 
     /**
      * Processes this test, when one of its tokens is encountered.
@@ -62,9 +128,8 @@ class Hotelsnl_Sniffs_Classes_ClassDeclarationSniff extends PEAR_Sniffs_Classes_
      */
     public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
     {
+        $this->braces($phpcsFile, $stackPtr);
         // We want all the errors from the PEAR standard, plus some of our own.
-        parent::process($phpcsFile, $stackPtr);
-
         $tokens = $phpcsFile->getTokens();
 
         /*
